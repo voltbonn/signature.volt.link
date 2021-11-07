0c7338fcd17144b0abb1ade5ba21ba23c43fd79b
dospringer <thomas.rosen@me.com>
2021-11-07 15:07:17 +0000

    dospringer published a site update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,13 +8,13 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "6e2a0c5b7a6af6fdbefe66206e2e26b8",
-"/": "6e2a0c5b7a6af6fdbefe66206e2e26b8",
+"index.html": "ded9441f002cca138ee8cb00d1a311e9",
+"/": "ded9441f002cca138ee8cb00d1a311e9",
 "manifest.json": "12718f6a94e95b8a16565ddea4e607a3",
 "Ubuntu-Bold.ttf": "e00e2a77dd88a8fe75573a5d993af76a",
 "version.json": "8663c355fc6b199a87e2b8cfd8daad39",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "14422d44e3885359131692589e03b74d",
+"assets/NOTICES": "b1b5cb921d89f02716cfc5c7483d5f6a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_widget_from_html_core/test/images/logo.png": "57838d52c318faff743130c3fcfae0c6",
 "assets/FontManifest.json": "734b361e2a1786a1c10bfc7452f2c8c8",
@@ -22,7 +22,7 @@ const RESOURCES = {
 "assets/assets/fonts/Ubuntu-Regular.ttf": "2505bfbd9bde14a7829cc8c242a0d25c",
 "assets/assets/images/volt_logo_purple.svg": "8fd76992f814ef2f7e6befaa63fbcf5e",
 "assets/AssetManifest.json": "8486c96890040fba54fbecde1c692157",
-"main.dart.js": "318e66b313f91be70d06cd85e5e0fe5c"
+"main.dart.js": "ac79df68712d2ae7dd194ce31c13d550"
 };
 
 // The application shell files that are downloaded before a service worker can
