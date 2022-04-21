b256c3409b898bcc75e1add2bc6e1983e38b16e9
dospringer <thomas.rosen@me.com>
2022-04-21 17:03:56 +0000

    dospringer published a site update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "e09fa801a6febb529937f4d106b4df24",
-"index.html": "0d6c655b4089202f6c39e8a55f8c8e39",
-"/": "0d6c655b4089202f6c39e8a55f8c8e39",
+  "main.dart.js": "14e71eedef4a45066d99eebd1462628a",
+"index.html": "918f4c38d5e5fdd3153cc31555ed783d",
+"/": "918f4c38d5e5fdd3153cc31555ed783d",
 "Ubuntu-Bold.ttf": "e00e2a77dd88a8fe75573a5d993af76a",
 "favicon.png": "b98a2fc9e00a0baaa82cd9ef31d16ba3",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
